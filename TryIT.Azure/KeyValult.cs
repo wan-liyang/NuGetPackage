c0a2e574a42e12e1c891de3330b0bbf05a4d360e
wan-liyang <51030647+wan-liyang@users.noreply.github.com>
2022-10-04 19:11:00 +0800

    add TryIT.Azure library

---
+++ TryIT.Azure/KeyValult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace TryIT.Azure
+{
+    /// <summary>
+    /// Azure KeyVault related function
+    /// </summary>
+    public class KeyVault
+    {
+        private SecretClient secretClient;
+
+        /// <summary>
+        /// initial instance with Vault Url
+        /// </summary>
+        /// <param name="vaultUrl"></param>
+        public KeyVault(string vaultUrl)
+        {
+            secretClient = new SecretClient(new Uri(vaultUrl), new DefaultAzureCredential());
+        }
+
+        /// <summary>
+        /// initial instance with Vault Url and Credential, e.g. <see cref="ClientCertificateCredential"/>
+        /// </summary>
+        /// <param name="vaultUrl"></param>
+        /// <param name="credential"></param>
+        public KeyVault(string vaultUrl, TokenCredential credential)
+        {
+            secretClient = new SecretClient(new Uri(vaultUrl), credential);
+        }
+
+        /// <summary>
+        /// get Secret value
+        /// </summary>
+        /// <param name="secretName"></param>
+        /// <returns></returns>
+        public string GetSecretValue(string secretName)
+        {
+            return secretClient.GetSecret(secretName).Value.Value;
+        }
+    }
+}
+
