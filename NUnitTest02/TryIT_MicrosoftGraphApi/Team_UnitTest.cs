f0145e36b8af4eeb0eac8263df9cd103273e67bd
wan-liyang <wan.liyang@outlook.com>
2024-01-25 11:36:04 +0800

    add TeamApi test case

---
+++ NUnitTest02/TryIT_MicrosoftGraphApi/Team_UnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TryIT.MicrosoftGraphApi.Model;
+using TryIT.MicrosoftGraphApi.Model.Team;
+using TryIT.MicrosoftGraphApi.MsGraphApi;
+
+namespace NUnitTest02.TryIT_MicrosoftGraphApi
+{
+    internal class Team_UnitTest
+    {
+        [Test]
+        public void Test()
+        {
+            MsGraphApiConfig apiConfig = new MsGraphApiConfig
+            {
+                //Proxy_Url = AppConfig.ProxyUrl,
+                Token = ""
+            };
+
+            TeamApi teamApi = new TeamApi(apiConfig, "O365-{teamName}-NCS");
+
+            teamApi.CreateTeam(new CreateTeamModel { DisplayName = "Test", Description = "Test" });
+        }
+    }
+}
