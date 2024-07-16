9b38b510ed3e2d83bb1db42dc1e6b59e5202c72c
wan-liyang <wan.liyang@outlook.com>
2024-07-16 14:09:06 +0800

    update DataFactoryApi to auto RefreshToken when expire

--- TryIT.MicrosoftGraphApi/AzureServiceManagement/DataFactoryApi.cs
+++ TryIT.MicrosoftGraphApi/AzureServiceManagement/DataFactoryApi.cs
@@ -1,4 +1,5 @@
-﻿using TryIT.MicrosoftGraphApi.Helper;
+﻿using System;
+using TryIT.MicrosoftGraphApi.Helper;
 using TryIT.MicrosoftGraphApi.HttpClientHelper;
 using TryIT.MicrosoftGraphApi.Model;
 using TryIT.MicrosoftGraphApi.MsGraphApi;
@@ -14,24 +15,55 @@ namespace TryIT.MicrosoftGraphApi.AzureServiceManagement
     {
         private static DataFactoryHelper _helper;
 
+        private DateTime TokenIssueOn;
+        private DateTime TokenExpireOn;
+        private ApiConfig _apiConfig;
         /// <summary>
         /// init application api with configuration
         /// </summary>
         /// <param name="config"></param>
         public DataFactoryApi(ApiConfig config)
         {
+            _apiConfig = config;
+
             TokenApi tokenApi = new TokenApi(new MsGraphApiConfig
             {
                 Proxy = config.Proxy
             });
 
+            this.TokenIssueOn = DateTime.Now;
             var tokenResponse = tokenApi.GetToken(config.TokenRequestInfo);
+            this.TokenExpireOn = this.TokenIssueOn.AddSeconds(tokenResponse.expires_in);
 
+            AssignGraphHelper(tokenResponse.access_token);
+        }
+
+        /// <summary>
+        /// refresh the token and helper if token expired
+        /// </summary>
+        private void RefreshToken()
+        {
+            if (this.TokenExpireOn < DateTime.Now.AddSeconds(-10))
+            {
+                TokenApi tokenApi = new TokenApi(new MsGraphApiConfig
+                {
+                    Proxy = _apiConfig.Proxy
+                });
+
+                var tokenResponse = tokenApi.GetToken(_apiConfig.TokenRequestInfo);
+                this.TokenExpireOn = this.TokenIssueOn.AddSeconds(tokenResponse.expires_in);
+
+                AssignGraphHelper(tokenResponse.access_token);
+            }
+        }
+
+        private void AssignGraphHelper(string access_token)
+        {
             MsGraphHelper graphHelper = new MsGraphHelper(new MsGraphApiConfig
             {
-                Proxy = config.Proxy,
-                Token = tokenResponse.access_token,
-                TimeoutSecond = config.TimeoutSecond,
+                Proxy = _apiConfig.Proxy,
+                Token = access_token,
+                TimeoutSecond = _apiConfig.TimeoutSecond,
             });
             _helper = new DataFactoryHelper(graphHelper.GetHttpClient());
         }
@@ -43,6 +75,7 @@ namespace TryIT.MicrosoftGraphApi.AzureServiceManagement
         /// <returns></returns>
         public CreateRunResponse.Response CreateRun(CreateRunRequest request)
         {
+            RefreshToken();
             return _helper.CreateRun(request);
         }
 
@@ -53,6 +86,7 @@ namespace TryIT.MicrosoftGraphApi.AzureServiceManagement
         /// <returns></returns>
         public GetPipelineRunsResponse.Response GetPipelineRun(GetPipelineRunRequest request)
         {
+            RefreshToken();
             return _helper.GetPipelineRun(request);
         }
     }
