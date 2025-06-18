61615614d7d43c691f6aa88f2bdfd5238250fbbc
Wan Li Yang (NCS) <liyang.wan2@ncs.co>
2025-06-18 10:27:30 +0800

    Add GetStatusCodeString extension for HttpResponseMessage
    Introduce a new extension method `GetStatusCodeString` to format HTTP status codes as strings in the `HttpResponseMessageExtension` class. Implement unit tests to validate the method's functionality and ensure proper exception handling for null responses.

---
+++ NUnitTest02/TryIT_ObjectExtension/HttpResponseMessageExtension_UnitTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TryIT.ObjectExtension;
+
+namespace NUnitTest02.TryIT_ObjectExtension
+{
+    internal class HttpResponseMessageExtension_UnitTest
+    {
+        [TestCase(HttpStatusCode.OK, "200 - OK")]
+        [TestCase(HttpStatusCode.Created, "201 - Created")]
+        [TestCase(HttpStatusCode.Accepted, "202 - Accepted")]
+        [TestCase(HttpStatusCode.NoContent, "204 - NoContent")]
+        [TestCase(HttpStatusCode.BadRequest, "400 - BadRequest")]
+        [TestCase(HttpStatusCode.Unauthorized, "401 - Unauthorized")]
+        [TestCase(HttpStatusCode.Forbidden, "403 - Forbidden")]
+        [TestCase(HttpStatusCode.NotFound, "404 - NotFound")]
+        [TestCase(HttpStatusCode.InternalServerError, "500 - InternalServerError")]
+        [TestCase(HttpStatusCode.NotImplemented, "501 - NotImplemented")]
+        [TestCase(HttpStatusCode.BadGateway, "502 - BadGateway")]
+        [TestCase(HttpStatusCode.ServiceUnavailable, "503 - ServiceUnavailable")]
+        public void GetStatusCode_ReturnsExpectedString_ForVariousStatusCodes(HttpStatusCode statusCode, string expected)
+        {
+            // Arrange
+            var response = new HttpResponseMessage(statusCode);
+
+            // Act
+            var result = response.GetStatusCodeString();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetStatusCode_ThrowsArgumentNullException_WhenResponseIsNull()
+        {
+            // Arrange
+            HttpResponseMessage response = null;
+
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentNullException>(() => response.GetStatusCodeString());
+            Assert.That(ex.Message, Does.Contain("Response cannot be null"));
+        }
+    }
+}
---
+++ TryIT.ObjectExtension/HttpResponseMessageExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace TryIT.ObjectExtension
+{
+    /// <summary>
+    /// extension class for HttpResponseMessage
+    /// </summary>
+    public static class HttpResponseMessageExtension
+    {
+        /// <summary>
+        /// Extension method to get the status code of an HttpResponseMessage as a formatted string, e.g. "200 - OK".
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string GetStatusCodeString(this System.Net.Http.HttpResponseMessage response)
+        {
+            if (response == null)
+            {
+                throw new ArgumentNullException(nameof(response), "Response cannot be null");
+            }
+
+            return $"{(int)response.StatusCode} - {response.StatusCode}";
+        }
+    }
+}
